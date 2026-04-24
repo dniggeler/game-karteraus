d1ef970e460bb1a0a9b5a390e2e71c63e2d908e4
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2026-04-24 07:56:31 +0200

    add git ignore file

--- src/server/Kartenreihen.Api/Program.cs
+++ src/server/Kartenreihen.Api/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("client", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("http://localhost:5173","http://localhost:5174")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
